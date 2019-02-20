4702348e85fe682c7af3452a83867065a3fd7198
David <David@David-PC>
2019-02-20 17:20:03 +0100

    SEEDS : Local , Rol , Sala. Inicialización con datos.

--- database/seeds/salaTableSeeder.php
+++ database/seeds/salaTableSeeder.php
@@ -1,16 +1,42 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Sala;
 
-class salaTableSeeder extends Seeder
-{
+class salaTableSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run() {
+
+        self::seedSala();
+        $this->command->info('Tabla sala inicializada con datos!');
     }
+
+    private function seedSala() {
+        DB::table('sala')->delete();
+        foreach ($this->arraySalas as $sala) {
+            $p = new Sala;
+            $p->capacity = $sala['capacity'];
+            $p->sala_name = $sala['sala_name'];
+            $p->save();
+        }
+    }
+
+    private $arraySalas = array(
+        array(
+            'capacity' => '1980',
+            'sala_name' => 'John Mayers',
+            'sala_photo' => 'photoooo'
+        ),
+        array(
+            'capacity' => '1900',
+            'sala_name' => 'Arnold Schuacheneger',
+            'sala_photo' => 'photooo'
+        )
+    );
+
 }
